69c05ea9de5a181793097cfcdabb52417dff6496
Elier <87112922+Oft3r@users.noreply.github.com>
2025-04-04 23:41:43 -0500

    Commit inicial

--- xian-web-wallet-main/templates/page_js/get-started.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('btn-get-started-create-wallet').addEventListener('click', function() {
-    changePage('create-wallet');
-});
-document.getElementById('btn-get-started-import-wallet').addEventListener('click', function() {
-    changePage('import-wallet');
-});
